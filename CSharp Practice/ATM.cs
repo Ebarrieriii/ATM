2f4272be1692b4fb1b7343cd97fd86bdea787c30
edward.barrier.iii@transcendentsoftware.com <edward.barrier.iii@transcendentsoftware.com>
2022-02-03 22:02:53 -0600

    Add project files.

---
+++ CSharp Practice/ATM.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class ATM
+    {
+        static void Main(string[] args)
+        {
+            bool login = true;
+            int loginError = 0;
+            int passwordAttempt = 0;
+            double balance = 0;
+
+            do
+            {
+                Console.WriteLine("Please enter your account number: \n");
+                string username = Console.ReadLine();
+                if (username.ToLower() == "64052")
+                {
+                    Console.Clear();
+
+                    do
+                    {
+                        Console.WriteLine("Please enter your password: \n");
+                        string password = Console.ReadLine();
+                        if (password.ToLower() == "ebarrier11")
+                        {
+                            do
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Welcome, Edward! \n \n");
+                                Console.WriteLine("Please select an option: \n" +
+                                    "a) Deposit \n" +
+                                    "b) Withdrawal \n" +
+                                    "c) View Balance \n" +
+                                    "d) Exit");
+                                string menu = Console.ReadLine();
+                                menu.ToLower();
+                                switch (menu)
+                                {
+                                    case "a":
+                                        Console.WriteLine("How much would you like to deposit?");
+                                        double addAmount = double.Parse(Console.ReadLine());
+                                        balance += addAmount;
+                                        Console.WriteLine($"You deposited {addAmount:c}. Press any key continue.");
+                                        Console.WriteLine($"New Balance: {balance:c}");
+                                        Console.ReadKey();
+                                        break;
+                                    case "b":
+                                        Console.WriteLine("How much would you like to withdrawal?");
+                                        double minusAmount = double.Parse(Console.ReadLine());
+                                        if (minusAmount > balance)
+                                        {
+                                            Console.WriteLine($"You may not withdraw {minusAmount:c}, as you only have {balance:c}.");
+                                        } else
+                                        {
+                                            balance -= minusAmount;
+                                            Console.WriteLine($"You withdraw {minusAmount:c}");
+                                            Console.WriteLine($"New Balance: {balance:c}");
+                                        }
+                                        Console.ReadKey();
+                                        break;
+                                    case "c":
+                                        Console.WriteLine($"Your balance is {balance:c}. Press any key to continue.");
+                                        Console.ReadKey();
+                                        break;
+                                    case "d":
+                                        Console.WriteLine("Goodbye! :)");
+                                        Console.ReadKey();
+                                        login = false;
+                                        break;
+                                }
+
+                            } while (login);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Password is incorrect. Please try again \n");
+                            passwordAttempt++;
+                        }
+                        if (passwordAttempt >= 3)
+                        {
+                            login = false;
+                        }
+                    }
+                    while (login == true);
+
+
+
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("That is not the correct username. Please try again \n");
+                    loginError++;
+                }
+            } while ((login == true) && (loginError < 3));
+        }
+    }
+}
---
+++ Problems/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ********** POWERS *********** // 
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(Math.Pow(i, 2));
+            }
+
+            // *********** FIZZBUZZ *********** //
+
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            // ********** Fibonacci ********** // 
+
+            int num1 = 0, num2 = 1;
+            int num3;
+            Console.WriteLine("Enter a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{num1} \n {num2}");
+
+            for (int i = 1; i <= userNumber; i++)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine(num3);
+                num1 = num2;
+                num2 = num3;
+            }
+
+            // ********** Interest Calculator ********** //
+
+            Console.WriteLine("Interest Calculator \n \n \n");
+            Console.WriteLine("Enter the beginning balance: ");
+            double balance = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the interest rate percentage: ");
+            double interestRate = double.Parse(Console.ReadLine());
+            Console.WriteLine("Amount of years: ");
+            double time = double.Parse(Console.ReadLine());
+            double amount = 0;
+            for (int i = 0; i <= time; i++)
+            {
+                amount = amount * (1 + interestRate);
+            }
+
+
+            Console.Clear();
+            Console.WriteLine($"Beginning Balance: {balance} \n" +
+                $"Interest Rate: {interestRate} \n" +
+                $"Total Time: {time} \n " +
+                $"Total Amount: {amount:c}");
+
+
+
+
+
+        }
+    }
+}
